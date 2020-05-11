bfd235ffe78f25bd6b8411010d4215223ebccbfe
blackplusy <osimeno@163.com>
2020-05-11 10:42:50 +0800

    Add files via upload

---
+++ 例子-0511-02.python的输入.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+'''
+name=input()
+print('your name is %s '% name)
+print(type(name))
+
+name=input('请输入您的法号！！！')
+print('im 法号'+name)
+'''
+a=int(input('a'))
+b=int(input('b'))
+print(a+b)
